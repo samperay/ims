20f342f1ca70f55aaa8f38aa8a82e09d6a6b3886
samperay <sunlnx@gmail.com>
2024-04-02 16:37:07 +0530

    modified running path

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ims
 inventory management system for infrastructure operations
+
+
+## Application start
+
+from the application directory i.e ims.. 
+
+```
+git clone git@github.com:samperay/ims.git
+
+# start the server
+uvicorn app.main:app --port 8000 --reload
+
+
+retrive list of all servers..
+http://127.0.0.1:8000/
+```
--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 # Description: This is the main file for the FastAPI application. It contains the code for the FastAPI application and the server inventory data.
 from fastapi import FastAPI
-# from pydantic import BaseModel, Field
-from routers import hosts as hosts_router
+from app.routers import server as server_router
 
 
 app=FastAPI()
 
-app.include_router(hosts_router.router)
+app.include_router(server_router.router)
